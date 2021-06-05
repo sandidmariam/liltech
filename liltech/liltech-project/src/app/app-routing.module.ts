e07186365c74eec78ca7d90260ce40175a6a5986
sandidmariam <52035910+sandidmariam@users.noreply.github.com>
2021-06-06 00:28:11 +0100

    new

--- liltech/liltech-project/src/app/app-routing.module.ts
+++ liltech/liltech-project/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GameComponent } from './game/game.component';
+import { TemplatesComponent } from './templates/templates.component';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot([
+    {path: 'game', component: GameComponent},
+    {path: 'index', component: TemplatesComponent},
+    {path: 'about', component: TemplatesComponent}
+  ]),
+],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- liltech/liltech-project/src/app/app.module.ts
+++ liltech/liltech-project/src/app/app.module.ts
@@ -4,15 +4,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TemplatesComponent } from './templates/templates.component';
+import { GameCardComponent } from './game-card/game-card.component';
+import { RestartDialogComponent } from './restart-dialog/restart-dialog.component';
+import { GameComponent } from './game/game.component';
+
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TemplatesComponent
+    TemplatesComponent,
+    GameCardComponent,
+    RestartDialogComponent,
+    GameComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
